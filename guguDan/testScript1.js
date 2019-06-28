acce2e3290a63ce23d9548e2145386b2c25c4db3
sunminkimmmm <minmax24@naver.com>
2019-06-28 14:04:06 +0900

    guguDan

--- guguDan/testScript1.js
+++ guguDan/testScript1.js
@@ -3,24 +3,37 @@ var num2 = Math.ceil(Math.random() * 9);
 var result = num1 * num2;
 
 var bodyValue = document.body;
+
+//문서 객체 생성
 var word = document.createElement('div');
+//생성한 태그 안의 글 
 word.textContent = String(num1) + '곱하기' + String(num2) + '는?';
+//<body>태그 안에 부착
 document.body.append(word);
 
+//폼 객체 생성
 var formValue = document.createElement('form');
+//<body>태그 안에 부착
 document.body.append(formValue);
 
+//입력폼 객체 생성
 var inputValue = document.createElement('input');
 inputValue.type = 'number';
+//<form>태그 안에 부착
 formValue.append(inputValue);
 
+//버튼 객체 생성
 var buttonValue = document.createElement('button');
 buttonValue.textContent='입력!';
+//<form>태그 안에 부착
 formValue.append(buttonValue);
 
+//문서 객체 생성
 var resultValue = document.createElement('div');
+//<body>태그 안에 부착
 document.body.append(resultValue);
 
+//폼 태그 안에 이벤트 발생시
 formValue.addEventListener('submit',function callback(e){
     e.preventDefault();
         if(result === Number(inputValue.value)){
@@ -34,7 +47,7 @@ formValue.addEventListener('submit',function callback(e){
     }else{
         resultValue.textContent = '땡';
         inputValue.value = '';
-        
+
     }
 
 })
